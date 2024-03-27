de1f1e11e7c65a875551b2c4ee98d01b6484a4e4
Elijah Kamali <94736197+littlestripes@users.noreply.github.com>
2024-03-27 13:26:45 -0500

    Complete lecture tasks

--- would-you-rather/src/App.vue
+++ would-you-rather/src/App.vue
@@ -1,30 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+import WouldYouRather from './components/WouldYouRather.vue'
+
+// question and two answers
+const wyrQuestion = ref('Would you rather be able to see new colors that no other people could see or be able to hear things that no other humans can hear?');
+const wyrAnswer1 = ref('Colors');
+const wyrAnswer2 = ref('Hearing');
+
+// for the user's selection
+const userSelection = ref('');
+
+function updateUserSelection(userChoice) {
+    userSelection.value = `Thanks! You chose ${userChoice}`;
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div id="app">
+
+        <h1>Would You Rather...</h1>
+
+        <would-you-rather
+            :question="wyrQuestion"
+            :answer1="wyrAnswer1"
+            :answer2="wyrAnswer2"
+            @answer-selected="updateUserSelection">
+        </would-you-rather>
+
+        {{ userSelection }}
+
+    </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#app {
+    background-color: grey;
 }
 </style>
---
+++ would-you-rather/src/components/WouldYouRather.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref, watch } from 'vue';
+
+defineProps({
+    question: String,
+    answer1: String,
+    answer2: String
+});
+
+const emit = defineEmits([
+    'answer-selected'
+]);
+
+const choice = ref('');
+
+/*function choiceMade() {*/
+    /*emit('answer-selected', choice.value);*/
+/*}*/
+
+watch(choice, () => {
+    emit('answer-selected', choice.value);
+});
+</script>
+
+<template>
+    <div id="wyr">
+        <h2>Make your choice!</h2>
+    
+        {{ question }}
+    
+        <input type="radio" id="answer-1" v-model="choice" :value="answer1">
+        <label for="answer-1">{{ answer1 }}</label>
+        <input type="radio" id="answer-2" v-model="choice" :value="answer2">
+        <label for="answer-2">{{ answer2 }}</label>
+    </div>
+</template>
+
+<style scoped>
+#wyr {
+    background-color: #444444;
+}
+</style>
