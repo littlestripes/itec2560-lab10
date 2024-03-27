29dc662b472c9d1f71fbd0374f0deebd0b6a0002
Elijah Kamali <94736197+littlestripes@users.noreply.github.com>
2024-03-27 16:52:16 -0500

    Add units handling options and logic
    - Added watch on 'useMetric' checkbox to dynamically update units between imperial and metric systems.
    - Modified 'calculateBMI' function to consider the selected units for calculation.
    - Updated UI to display units based on the selected system.
    - Implemented rudimentary input validation to ensure non-empty height and weight values before emitting stats.

--- bmi/src/App.vue
+++ bmi/src/App.vue
@@ -1,24 +1,57 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 import BodyMassIndexForm from './components/BodyMassIndexForm.vue'
 
 const bmi = ref('');
 
+// default to imperial units since v-model can't read an initial checked state
+const heightUnit = ref('inches');
+const weightUnit = ref('pounds');
+
+const useMetric = ref('');
+
 function calculateBMI(height, weight) {
-    bmi.value = (weight / ( height ** 2 )).toFixed(2);
+    if (useMetric.value) {
+        bmi.value = (weight / (height ** 2)).toFixed(2);
+    } else {
+        bmi.value = ((weight / (height ** 2)) * 730).toFixed(2);
+    }
 }
+
+// keep an eye on the checkbox and update units as necessary
+watch(useMetric, (useMetric) => {
+    if (useMetric) {
+        heightUnit.value = 'meters';
+        weightUnit.value = 'kilos';
+    } else {
+        heightUnit.value = 'inches';
+        weightUnit.value = 'pounds';
+    }
+
+    // clears the old value so the message disappears
+    bmi.value = '';
+})
 </script>
 
 <template>
 
-    <h1>Body Mass Index Calculator</h1>
+    <div id="main">
+        <h1>Body Mass Index Calculator</h1>
+    
+        <BodyMassIndexForm
+            :heightUnit="heightUnit"
+            :weightUnit="weightUnit"
+            @stats-entered="calculateBMI">
+        </BodyMassIndexForm>
+    
+        <h3 v-if="bmi">BMI is {{ bmi }}</h3>
+    </div>
 
-    <BodyMassIndexForm
-        @stats-entered="calculateBMI">
-    </BodyMassIndexForm>
-
-    <h3 v-if="bmi">BMI is {{ bmi }}</h3>
+    <div id="options">
+        <h3>Options</h3>
+        <input type="checkbox" checked v-model="useMetric" name="units"><label for="units"> Use metric?</label>
+    </div>
 
 </template>
 
--- bmi/src/components/BodyMassIndexForm.vue
+++ bmi/src/components/BodyMassIndexForm.vue
@@ -3,7 +3,9 @@ import { ref } from 'vue';
 
 defineProps({
     height: Number,
-    weight: Number
+    weight: Number,
+    heightUnit: String,
+    weightUnit: String
 });
 
 const emit = defineEmits([
@@ -14,17 +16,22 @@ const height = ref();
 const weight = ref();
 
 function statsEntered() {
-    emit('stats-entered', height.value, weight.value);
-    console.log('Height and weight entered, emitting to App.vue...');
+    // check to make sure the boxes aren't empty, rudimentary input validation
+    if (height.value !== undefined && weight.value !== undefined) {
+        emit('stats-entered', height.value, weight.value);
+        console.log('Height and weight entered, emitting to App.vue...');
+    }
 }
 </script>
 
 <template>
     <h2>Enter your height and weight</h2>
     <p>
-        <label for="bmi-height">Height (in meters) </label><input name="bmi-height" v-model="height">
+        <label for="bmi-height">Height (in {{ heightUnit }}) </label>
+        <input name="bmi-height" v-model="height">
         <br>
-        <label for="bmi-weight">Weight (in kilos) </label><input name="bmi-weight" v-model="weight">
+        <label for="bmi-weight">Weight (in {{ weightUnit }}) </label>
+        <input name="bmi-weight" v-model="weight">
     </p>
     <button @click="statsEntered">Calculate</button>
 </template>
